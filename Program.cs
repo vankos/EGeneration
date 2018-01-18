12eb7cc75148bc133a0dac7afc04da6a0121265e
vankos <vano111ru@yandex.ru>
2018-01-18 16:53:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGeneration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0:d}", 0b10,11111111111);
+        }
+    }
+}

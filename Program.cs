976bd23e79b48ae39883125c4a6bf1d192e002b0
vankos <vano111ru@yandex.ru>
2018-01-20 13:45:05 +0300

    first

--- Program.cs
+++ Program.cs
@@ -10,7 +10,25 @@ namespace EGeneration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0:d}", 0b10,11111111111);
+            Console.WriteLine("What accuracy you want?(numbers after the decimal point): ");
+            int NumberOfNumbers =Convert.ToInt32( Console.ReadLine())+3;
+            Console.WriteLine("{0}", FindEiler(NumberOfNumbers));
+            Console.ReadLine();
+        }
+
+       static decimal FindEiler(int n)
+        {
+            decimal e = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                decimal Factorial = 1;
+                for (int j = 1; j <= i; j++)
+                {
+                    Factorial *= j;
+                }
+                e += 1 /Factorial;
+            }
+            return e;
         }
     }
 }

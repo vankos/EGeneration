045d310f992d109fabaf1337785ea5e31f759664
vankos <vano111ru@yandex.ru>
2018-01-28 15:04:22 +0300

    no restriction for accuracy

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace EGeneration
 {
@@ -10,22 +11,24 @@ namespace EGeneration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What numbers after the decimal point you want?(max:22):");
+            Console.WriteLine("What numbers after the decimal point you want?:");
             string userInputString = Console.ReadLine();
             int NumberOfNumbers = ValidateInput(userInputString);
             //i have to use substring meethod for cut exactly necessary amount of digit 
-            Console.WriteLine("e={0}", FindEiler(NumberOfNumbers+1).ToString().Substring(0,NumberOfNumbers-1));
+            Console.WriteLine("e={0}", FindEiler(NumberOfNumbers + 1).ToString().Substring(0,NumberOfNumbers-2).Insert(1,","));
             Console.ReadLine();
         }
 
-       static decimal FindEiler(int n)
+       static BigInteger FindEiler(int n)
         {
-            decimal e = 1;
-            decimal Factorial = 1;
+            // BigInteger works only with non-float values , so i had to multiply divended valu on number of requered digits after comma
+            BigInteger e = BigInteger.Pow(10, n);
+            BigInteger factorial = 1;
             for (int i = 1; i <= n; i++)
             {
-                Factorial *= i;
-                e += 1 /Factorial;
+                factorial = BigInteger.Multiply(factorial,i);
+                BigInteger  division = BigInteger.Divide(BigInteger.Pow(10, n), factorial);
+                e = BigInteger.Add(e,division);
             }
             return e;
         }
@@ -33,7 +36,7 @@ namespace EGeneration
         private static int ValidateInput(string userInputString)
         {
             int numberOfNumbers;
-            while ((!int.TryParse(userInputString, out numberOfNumbers)) || (numberOfNumbers < 0)||(numberOfNumbers>22))
+            while ((!int.TryParse(userInputString, out numberOfNumbers)) || (numberOfNumbers < 0))
             {
                 Console.Write("Write correct value:");
                 userInputString = Console.ReadLine();
